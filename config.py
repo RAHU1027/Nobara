5bb7508fed16ece71da79f2b5c4a745b6c8550f2
RAHU1027 <sanjitrai88888888@gmail.com>
2026-06-12 18:47:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 class config:
     
     #Client
-    API_ID = 0
-    API_HASH = "0"
+    API_ID = 21552435
+    API_HASH = "5b108bd2fdd31c0c34bc65f24a5216a0"
     BOT_TOKEN = "0:0-0"
-    BOT_NAME = "Your_Bot_Name"
-    BOT_USERNAME = "Your_Bot_Username"
-    BOT_ID = 0
+    BOT_NAME = "8283404104:AAHWezqc3mNKmP87HmE-zdW0f76iEvP5UZo"
+    BOT_USERNAME = "@Kushalmusicbot"
+    BOT_ID = 8283404104
     WORKERS = 30
     MAX_MESSAGE_CACHE_SIZE = 100
     MAX_CONCURRENT_TRANSMISSIONS = 10
@@ -19,13 +19,13 @@ class config:
 
     #Info
     BOT_VERSION = "x"
-    OWNER_ID = 5630057244
+    OWNER_ID = 6632236983
     OWNER_USERNAME = "senpaiii10"
-    SUPPORT_CHAT = -1002069412308
+    SUPPORT_CHAT = -1002547223008
     SUPPORT_CHAT_USERNAME = "Domihoes"
     SUPPORT_CHAT_LINK = "https://t.me/Domihoes"
-    LOG_CHANNEL = -1001625036774
-    ERROR_LOG_CHANNEL = -1001625036774
+    LOG_CHANNEL = -1002547223008
+    ERROR_LOG_CHANNEL = -1002547223008
     DOWNLOAD_LOCATION = "./downloads"
     COMMAND_PREFIXES = ["/" , "!" , "." , "#" , "$" , "%" , "&" , "?"] 
     CMD_STARTERS = "/.!&#%$"
@@ -48,8 +48,8 @@ class config:
     NEWS_URL = "https://sugoi-api.vercel.app/news?keyword={}"
     shayri_api_url = "https://hindi-quotes.vercel.app/random"
     BASE_URL = "https://api.waifu.pics"
-    Movie_Api = "5d3274c3bb08b4276482436c8444abc0"
-    Movie_RAC = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDMyNzRjM2JiMDhiNDI3NjQ4MjQzNmM4NDQ0YWJjMCIsIm5iZiI6MTczMzIyMjgxMy42OTQwMDAyLCJzdWIiOiI2NzRlZTE5ZDJjZTRjZTdkZDYwOTU2YjAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.aXfQw0_CRrKl2iSJd9tFE1TVbWWVYNgysWkUVlwzyRg"
+    Movie_Api = "5b108bd2fdd31c0c34bc65f24a5216a0"
+    Movie_RAC = "BQFI3TMAPhZ-hZII7FJtpJFCXtQjfMqG3TYSNMKl8RZIIBqbfdGzwFn7l4KFnwVfADGz_3ej2ZTBYcSv-HXCbYqd1goaLQKPlMaAPyz08wBTm-UqglCY-7UuN22lmEI4cF6xUs_3pdh8YPaDw3eM2NUCkDzFyzgW_HekAY2ufH8_3dNYfpBopshYUmJTFiGm4ie0a_5741UjnoI5M_WeHgpDjkAsD8UFiWdCl9yS27PUeKTINa7yhh7JuQHunCtJDKLViYwrcCTnBz0oMQi-DBhqJ8znqidhcemUtOg1Z31391MuVU3_sV3V4mIlsMtonjF79PYDecNECYJxlXsKKVoERXU0bQAAAAHTR-i_AA"
     Pokedex = "https://sugoi-api.vercel.app/pokemon?name={name_or_id}"
     OPENAI_KEY = "xx" #Get From Open Ai's Website
     LYRICS_GENIUS_TOKEN = "x-x" #Get From Genius.com
