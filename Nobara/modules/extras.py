bffcdffcbaa851c24ee1362ff13e4289a53c5e04
RAHU1027 <sanjitrai88888888@gmail.com>
2026-06-12 19:54:58 +0530

    Update extras.py

--- Nobara/modules/extras.py
+++ Nobara/modules/extras.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 import aiohttp
 import random
+import re
 from Nobara import app
 from Nobara.vars import FLIRT as FLIRT_STRINGS , TOSS as TOSS_STRINGS , EYES , MOUTHS , EARS , DECIDE as DECIDE_STRINGS , weebyfont , normiefont 
 from config import config
@@ -41,7 +42,7 @@ async def hyperlink_command(client: Client, message: Message):
         await message.reply_text("𝖨𝗇𝗏𝖺𝗅𝗂𝖽 𝖿𝗈𝗋𝗆𝖺𝗍! 𝖴𝗌𝖾: /𝗁𝗒𝗉𝖾𝗋𝗅𝗂𝗇𝗄 <𝗍𝖾𝗑𝗍> <𝗅𝗂𝗇𝗄>")
 
 @app.on_message(filters.command("joke" , config.COMMAND_PREFIXES))
-@app.on_message(filters.regex(r"(?i)^Yumeko Ek Joke Sunao$"))
+@app.on_message(filters.regex(r"^Yumeko Ek Joke Sunao$", flags=re.IGNORECASE))
 @error
 @save
 async def joke(client: Client, message: Message):
@@ -69,14 +70,14 @@ async def roll(client: Client, message: Message):
     await message.reply_text(random.randint(1, 6))
 
 @app.on_message(filters.command("flirt" , config.COMMAND_PREFIXES))
-@app.on_message(filters.regex(r"(?i)^Yumeko flirt$"))
+@app.on_message(filters.regex(r"^Yumeko flirt$", flags=re.IGNORECASE))
 @error
 @save
 async def flirt(client: Client, message: Message):
     await message.reply_text(random.choice(FLIRT_STRINGS))
 
 @app.on_message(filters.command("toss" , config.COMMAND_PREFIXES))
-@app.on_message(filters.regex(r"(?i)^Yumeko toss$"))
+@app.on_message(filters.regex(r"^Yumeko toss$", flags=re.IGNORECASE))
 @error
 @save
 async def toss(client: Client, message: Message):
@@ -105,7 +106,7 @@ async def rlg(client: Client, message: Message):
     await message.reply_text(face)
 
 @app.on_message(filters.command("decide" , config.COMMAND_PREFIXES))
-@app.on_message(filters.regex(r"(?i)^Yumeko decide$"))
+@app.on_message(filters.regex(r"^Yumeko decide$", flags=re.IGNORECASE))
 @error
 @save
 async def decide(client: Client, message: Message):
